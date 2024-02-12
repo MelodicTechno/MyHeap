58d1c33c86f713460ca5ad7ab9e631b6154b3172
MelodicTechno <617179025@qq.com>
2024-02-12 23:32:47 +0800

    Create MinHeapTester.java

---
+++ src/MinHeapTester.java
@@ -0,0 +1,20 @@
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Random;
+
+public class MinHeapTester {
+    public static void main(String[] args) {
+        MinHeap<Integer> minHeap = new MinHeap<>();
+        Random integersMaker = new Random();
+        Queue<Integer> sortedResult = new LinkedList<>();
+        for (int i = 0; i < 10; i++) {
+            int toBeInserted = integersMaker.nextInt(0, 50);
+            minHeap.insert(toBeInserted);
+        }
+        System.out.println(minHeap.peek());
+        while (!minHeap.isEmpty()) {
+            sortedResult.offer(minHeap.delete());
+        }
+        System.out.println(sortedResult);
+    }
+}
