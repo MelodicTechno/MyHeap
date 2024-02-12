191b60b3c4ffa13cb52dcf9ff2033dec9b0a2304
MelodicTechno <617179025@qq.com>
2024-02-12 23:32:41 +0800

    Create HeapSort.java

---
+++ src/HeapSort.java
@@ -0,0 +1,19 @@
+import java.util.Queue;
+
+public class HeapSort<E extends Comparable<E>> {
+    private final MinHeap<E> minHeap = new MinHeap<>();
+    // a sort algorithm using my min heap. not that elegant!
+    public void sort(E[] dataToBeSorted) {
+        for (E e : dataToBeSorted) {
+            minHeap.insert(e);
+        }
+        for (int i = 0; i < dataToBeSorted.length; i++) {
+            dataToBeSorted[i] = minHeap.delete();
+        }
+    }
+    // toString
+    @Override
+    public String toString() {
+        return "HeapSort";
+    }
+}
