bdaa6abb8c5d021a759ed25e6f1a79a08aeb03e3
MelodicTechno <617179025@qq.com>
2024-02-13 21:00:45 +0800

    Update TripleHeap.java

--- src/TripleHeap.java
+++ src/TripleHeap.java
@@ -1,6 +1,6 @@
 import java.util.ArrayList;
 
-public class TripleHeap<E extends Comparable<E>> extends MinHeap<E> {
+public class TripleHeap<E extends Comparable<E>> {
     // using an array to implement triple tree
     private final ArrayList<E> heap;
     // constructor
@@ -8,8 +8,21 @@ public class TripleHeap<E extends Comparable<E>> extends MinHeap<E> {
         this.heap = new ArrayList<>();
     }
 
-    @Override
+    // get the top of the heap
+    public E peek() {
+        return heap.get(0);
+    }
+    public boolean isEmpty() {
+        return heap.isEmpty();
+    }
+    public void insert(E element) {
+        heap.add(element);
+        upHeapify(heap.size() - 1);
+        // rearrange(heap.size() - 1);
+    }
+
     // heapification from index i
+    // it is different from binary tree
     public void upHeapify(int i) {
         while (true) {
             int parentIndex = getParentIndex(i);
@@ -21,7 +34,6 @@ public class TripleHeap<E extends Comparable<E>> extends MinHeap<E> {
         }
     }
 
-    @Override
     // heapify downwards
     public void downHeapify(int i) {
         while (true) {
@@ -45,7 +57,7 @@ public class TripleHeap<E extends Comparable<E>> extends MinHeap<E> {
             i = minIndex;
         }
     }
-    @Override
+
     // deletion of heap
     public E delete() {
         // if empty
@@ -57,19 +69,59 @@ public class TripleHeap<E extends Comparable<E>> extends MinHeap<E> {
         downHeapify(0);
         return removedElement;
     }
-    @Override
+
     public int getLeftIndex(int index) {
         return 3 * index + 1;
     }
     public int getMidIndex(int index) {
         return 3 * index + 2;
     }
-    @Override
+
     public int getRightIndex(int index) {
         return 3 * index + 3;
     }
-    @Override
+
     public int getParentIndex(int index) {
         return (index - 1) / 3;
+    }
+
+    // swap two nodes
+    public void swap(int index1, int index2) {
+        E NodeForStore = heap.get(index1);
+        heap.set(index1, heap.get(index2));
+        heap.set(index2, NodeForStore);
+    }
+    public ArrayList<E> getHeap() {
+        return heap;
+    }
+    // rearrange the 3 nodes
+    public void rearrange(int index) {
+        int parentIndex = getParentIndex(index);
+        int leftSisterIndex = getLeftIndex(parentIndex);
+        int midSisterIndex = getMidIndex(parentIndex);
+        int rightSisterIndex = getRightIndex(parentIndex);
+        ArrayList<E> sisters = new ArrayList<>();
+        if (leftSisterIndex > 0 && leftSisterIndex < heap.size()) {
+            sisters.add(heap.get(leftSisterIndex));
+        }
+        if (midSisterIndex > 0 && midSisterIndex < heap.size()) {
+            sisters.add(heap.get(midSisterIndex));
+        }
+        if (rightSisterIndex > 0 && rightSisterIndex < heap.size()) {
+            sisters.add(heap.get(rightSisterIndex));
+        }
+        if (sisters.size() == 2) {
+            InsertionSort<E> insertionSort = new InsertionSort<>(sisters);
+            insertionSort.sort();
+            heap.set(leftSisterIndex, sisters.get(0));
+            heap.set(midSisterIndex, sisters.get(1));
+        }
+        if (sisters.size() == 3) {
+            InsertionSort<E> insertionSort = new InsertionSort<>(sisters);
+            insertionSort.sort();
+            heap.set(leftSisterIndex, sisters.get(0));
+            heap.set(midSisterIndex, sisters.get(1));
+            heap.set(rightSisterIndex, sisters.get(2));
+        }
     }
 }
