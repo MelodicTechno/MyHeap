0d65e1c9898a60b65e0b182c03716d859efc1103
MelodicTechno <617179025@qq.com>
2024-02-13 23:05:35 +0800

    Update TripleHeap.java

--- src/TripleHeap.java
+++ src/TripleHeap.java
@@ -8,31 +8,7 @@ public class TripleHeap<E extends Comparable<E>> extends MinHeap<E> {
         this.heap = new ArrayList<>();
     }
 
-    // get the top of the heap
-    public E peek() {
-        return heap.get(0);
-    }
-    public boolean isEmpty() {
-        return heap.isEmpty();
-    }
-    public void insert(E element) {
-        heap.add(element);
-        upHeapify(heap.size() - 1);
-    }
-
-    // heapification from index i
-    // it is different from binary tree
-    public void upHeapify(int i) {
-        while (true) {
-            int parentIndex = getParentIndex(i);
-            if (parentIndex < 0 || heap.get(i).compareTo(heap.get(parentIndex)) >= 0) {
-                break;
-            }
-            swap(i, parentIndex);
-            i = parentIndex;
-        }
-    }
-
+    @Override
     // heapify downwards
     public void downHeapify(int i) {
         while (true) {
@@ -57,37 +33,22 @@ public class TripleHeap<E extends Comparable<E>> extends MinHeap<E> {
         }
     }
 
-    // deletion of heap
-    public E delete() {
-        // if empty
-        if (heap.isEmpty()) {
-            throw new IndexOutOfBoundsException();
-        }
-        swap(0, heap.size() - 1);
-        E removedElement = heap.remove(heap.size() - 1);
-        downHeapify(0);
-        return removedElement;
-    }
-
+    @Override
     public int getLeftIndex(int index) {
         return 3 * index + 1;
     }
+
     public int getMidIndex(int index) {
         return 3 * index + 2;
     }
 
+    @Override
     public int getRightIndex(int index) {
         return 3 * index + 3;
     }
 
+    @Override
     public int getParentIndex(int index) {
         return (index - 1) / 3;
-    }
-
-    // swap two nodes
-    public void swap(int index1, int index2) {
-        E NodeForStore = heap.get(index1);
-        heap.set(index1, heap.get(index2));
-        heap.set(index2, NodeForStore);
     }
 }
