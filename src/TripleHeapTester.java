2490ed4b71a41d30541941ca0e17c3955771e362
MelodicTechno <617179025@qq.com>
2024-02-13 22:49:26 +0800

    Update TripleHeapTester.java

--- src/TripleHeapTester.java
+++ src/TripleHeapTester.java
@@ -9,14 +9,11 @@ public class TripleHeapTester {
         Queue<Integer> sortedResult = new LinkedList<>();
         for (int i = 0; i < 10; i++) {
             int toBeInserted = integersMaker.nextInt(0, 50);
-            System.out.println(toBeInserted);
             tripleHeap.insert(toBeInserted);
         }
         System.out.println(tripleHeap.peek());
-        System.out.println(tripleHeap.getHeap());
-        while (tripleHeap.isEmpty()) {
+        while (!tripleHeap.isEmpty()) {
             int deletedElement = tripleHeap.delete();
-            System.out.println(deletedElement);
             sortedResult.offer(deletedElement);
         }
         System.out.println(sortedResult);
