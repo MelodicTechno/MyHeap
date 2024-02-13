c56974860b142b4e7d9ae3eb58da28a22d4fd63a
MelodicTechno <617179025@qq.com>
2024-02-13 21:00:40 +0800

    Create InsertionSort.java

---
+++ src/InsertionSort.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+
+public class InsertionSort<E extends Comparable<E>> {
+    private final ArrayList<E> dataToBeSorted;
+    public InsertionSort(ArrayList<E> dataToBeSorted) {
+        this.dataToBeSorted = dataToBeSorted;
+    }
+    public void sort() {
+        for (int i = 1; i < dataToBeSorted.size(); i++) {
+            for (int j = i; j > 0 && dataToBeSorted.get(j).compareTo(dataToBeSorted.get(j - 1)) > 0; j--) {
+                swap(j, j - 1);
+            }
+        }
+    }
+    public void swap(int index1, int index2) {
+        E elementForStore = dataToBeSorted.get(index1);
+        dataToBeSorted.set(index1, dataToBeSorted.get(index2));
+        dataToBeSorted.set(index2, elementForStore);
+    }
+}
